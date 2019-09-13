9087db8a293e2839fd10de49a175b8dd275cabba
Alberto <155007+Veilkrand@users.noreply.github.com>
2019-09-13 20:56:55 +0900

    relocated package

--- test_motors.py
+++ test_motors.py
@@ -5,4 +5,8 @@ RIGHT_TRIM = 0
 
 robot = Robot4WD(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM, left_id1=1, right_id1=2, left_id2=3, right_id2=4)
 
-robot.forward(50, 1)
+robot.forward(50, 5)
+
+#robot.left(50, 5)
+
+#robot.right(50, 5)
