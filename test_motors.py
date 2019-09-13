28def7ee780d64e3171b95e6de240d7a0971f137
Alberto <155007+Veilkrand@users.noreply.github.com>
2019-09-13 20:29:58 +0900

    relocated package

---
+++ test_motors.py
@@ -0,0 +1,8 @@
+from Robot4WD.Robot4WD import Robot4WD
+
+LEFT_TRIM = 0
+RIGHT_TRIM = 0
+
+robot = Robot4WD(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM,left_id1=1,right_id1=2,left_id2=3,right_id2=4)
+
+robot.forward(50, 1)
