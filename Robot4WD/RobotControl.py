957eac0d3281c0a1b3a86ecf886723b52414ca6e
Alberto <155007+Veilkrand@users.noreply.github.com>
2021-02-24 21:47:17 +0900

    trims

--- Robot4WD/RobotControl.py
+++ Robot4WD/RobotControl.py
@@ -3,16 +3,16 @@ from Robot4WD.Robot4WD import Robot4WD
 
 class RobotControl:
 
-    LEFT_TRIM = 0
-    RIGHT_TRIM = 0
-    MIN_SPEED = 0.1  # [0, 1] absolute to start moving forward
+    LEFT1_TRIM = 0
+    LEFT2_TRIM = 0
+    RIGHT1_TRIM = 0
+    RIGHT2_TRIM = 0
+    MIN_SPEED = 0.15  # [0, 1] absolute to start moving forward
     MIN_STEERING = 0.1  # [0, 1] Absolute to start turning
     MIN_SPEED_STEERING = 0.6  # [0, 1] absolute o switch from turning to steering
-    robot = Robot4WD(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM,
-                     left_id1=1,
-                     left_id2=2,
-                     right_id1=3,
-                     right_id2=4)
+    robot = Robot4WD(left1_trim=LEFT1_TRIM, right1_trim=RIGHT1_TRIM,
+                     left2_trim=LEFT2_TRIM, right2_trim=RIGHT2_TRIM,
+                     )
 
     def __init__(self):
         pass
